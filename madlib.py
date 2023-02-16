5ce7bf188f56a774315064768b60fe598ec8b759
AnanthanenDinesh <125575527+AnanthanenDinesh@users.noreply.github.com>
2023-02-16 19:38:01 +0530

    Add files via upload

---
+++ madlib.py
@@ -0,0 +1,11 @@
+name =input("name")
+city =input("city")
+country= input("country")
+city2= input("city2")
+group=input("group")
+hobby1=input("h1: ")
+hobby2=input("h2:")
+
+madlib = f"I am {name}, basically, I am from {city} {country} but currently staying in {city2} with my family. I have completed my Bachelor's in {group} . I like to {hobby1}and {hobby2}."
+
+print(madlib)
